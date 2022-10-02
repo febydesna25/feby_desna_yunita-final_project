0bdba686ab58e82d89ff8d1d7de28d82444b65e3
Feby Desna Yunita <febydesna@gmail.com>
2022-10-02 14:06:37 +0700

    initialisasi file configuration tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors:{
+        tema: '#2c5364',
+        light: '#e5e7eb',
+        tema2: '#203a43',
+        abu: '#64748b',
+      }
+    },
+  },
+  plugins: [],
+}
